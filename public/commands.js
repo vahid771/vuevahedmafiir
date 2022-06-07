de2f91bf183e595c06705b74d04b4efd7aaf057c
Vahed Mafi <vahid771@gmail.com>
2022-06-07 14:13:52 +0430

    bug fixed

--- public/commands.js
+++ public/commands.js
@@ -1,10 +1,3 @@
-/*
- * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
- * See LICENSE in the project root for license information.
- */
-
-/* global global, Office, self, window */
-
 Office.onReady(() => {
   // If needed, Office.js is ready to be called
 });
@@ -21,10 +14,10 @@ function action(event) {
     persistent: true,
   };
 
-  // Show a notification message
   Office.context.mailbox.item.notificationMessages.replaceAsync("action", message);
 
-  // Be sure to indicate when the add-in command function is complete
+  alert("commands.js Action");
+
   event.completed();
 }
 
@@ -40,5 +33,4 @@ function getGlobal() {
 
 const g = getGlobal();
 
-// The add-in command functions need to be available in global scope
 g.action = action;
--- public/taskpane.js
+++ public/taskpane.js
@@ -1,10 +1,3 @@
-/*
- * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
- * See LICENSE in the project root for license information.
- */
-
-/* global document, Office */
-
 Office.onReady((info) => {
   if (info.host === Office.HostType.Outlook) {
     document.getElementById("sideload-msg").style.display = "none";
@@ -14,7 +7,5 @@ Office.onReady((info) => {
 });
 
 export async function run() {
-  /**
-   * Insert your Outlook code here
-   */
+  alert("taskpane.js Action");
 }
